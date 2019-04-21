eb75a5d886e8fd5beeb1867bf5e05f466c5afb33
Stephen-hj <1364115532@qq.com>
2019-04-21 12:10:39 +0800

    2014年

--- 2013_Java_B/_01_世纪末的星期.java
+++ 2013_Java_B/_01_世纪末的星期.java
@@ -4,17 +4,17 @@ import java.util.Calendar;
 标题: 世纪末的星期
 
 
-    曾有邪教称1999年12月31日是世界末日。当然该谣言已经不攻自破。
+    曾有邪教�?999�?2�?1日是世界末日。当然该谣言已经不攻自破�?
 
-    还有人称今后的某个世纪末的12月31日，如果是星期一则会....
+    还有人称今后的某个世纪末�?2�?1日，如果是星期一则会....
 
-    有趣的是，任何一个世纪末的年份的12月31日都不可能是星期一!!
+    有趣的是，任何一个世纪末的年份的12�?1日都不可能是星期一!!
 
     于是，“谣言制造商”又修改为星期日......
 
-    1999年的12月31日是星期五，请问：未来哪一个离我们最近的一个世纪末年（即xx99年）的12月31日正好是星期天（即星期日）？
+    1999年的12�?1日是星期五，请问：未来哪一个离我们最近的一个世纪末年（即xx99年）�?2�?1日正好是星期天（即星期日）？
 
-    请回答该年份（只写这个4位整数，不要写12月31等多余信息）
+    请回答该年份（只写这�?位整数，不要�?2�?1等多余信息）
 
  */
 public class _01_世纪末的星期 {
@@ -22,7 +22,7 @@ public class _01_世纪末的星期 {
     Calendar calendar = Calendar.getInstance();
     for (int year = 1999; year < 10000; year+=100) {
       calendar.set(Calendar.YEAR,year);
-      calendar.set(Calendar.MONTH,11);//12月
+      calendar.set(Calendar.MONTH,11);//12�?
       calendar.set(Calendar.DAY_OF_MONTH,31);
       System.out.println(year+" "+calendar.get(Calendar.DAY_OF_WEEK));
       if(calendar.get(Calendar.DAY_OF_WEEK)==1){
